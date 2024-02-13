6d28cf9721cbfdfadb5032a4703255d9c30feea7
pablo <pabloynciorivera@gmail.com>
2024-02-13 00:57:37 -0500

    npm format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blank-backend
+
 base de un backend con node &amp; docker
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const cors = require("cors");
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,24 +10,26 @@ app.use(express.json());
 app.use(cors());
 
 // Rutas
-app.get('/', (req, res) => {
-    res.send('¡Bienvenido a mi aplicación backend con Express!');
+app.get("/", (req, res) => {
+  res.send("¡Bienvenido a mi aplicación backend con Express!");
 });
 
 // Ruta de ejemplo con validación de entrada
-app.post('/ejemplo', [
-    body('nombre').isLength({ min: 5 }).withMessage('El nombre debe tener al menos 5 caracteres')
-], (req, res) => {
+app.post(
+  "/ejemplo",
+  [body("nombre").isLength({ min: 5 }).withMessage("El nombre debe tener al menos 5 caracteres")],
+  (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { nombre } = req.body;
     res.json({ mensaje: `Hola, ${nombre}! Este es un ejemplo de ruta con validación de entrada.` });
-});
+  },
+);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
